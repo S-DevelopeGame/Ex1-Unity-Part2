416628bc63dc9eaad273a3bdd76a7b17027801e1
snir1551 <snir1552@gmail.com>
2021-11-02 18:10:08 +0200

    update

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -6,9 +6,13 @@ public class MovementPlayer : MonoBehaviour
 {
     [Tooltip("speed movement of the object")]
     [SerializeField] private float speed;
-    private Vector3 movement;
+
+    [Tooltip("Animator of the player")]
     [SerializeField] private Animator animator;
+
+    //Does the player look to the right
     private bool rightFace = true;
+    // save the speed of the player
     private float oldSpeed = 0;
     // Start is called before the first frame update
     void Start()
@@ -21,20 +25,25 @@ public class MovementPlayer : MonoBehaviour
     {
         float move = Input.GetAxis("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(move));
-        Debug.Log("move: " + move);
+        Debug.Log("move: " + move); // test of move
 
-        if(move < 0 && rightFace)
+        //If the player moves to the left and his face to the right
+        if (move < 0 && rightFace)
         {
-            transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
-            rightFace = false;
+            transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);//turn him 180 degree
+            rightFace = false;//he is not looking to the right
         }
-        else if(move > 0 && !rightFace)
+
+        //If the player moves to the right and his face to the left
+        else if (move > 0 && !rightFace)
         {
-            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-            rightFace = true;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);//turn him 180 degree
+            rightFace = true;//he is looking to the right
         }
-        transform.Translate(move*Time.deltaTime*speed, 0, 0);
-        if (transform.position.x < -10.300 && !rightFace || transform.position.x > 10.300 && rightFace)
+        transform.Translate(move*Time.deltaTime*speed, 0, 0); // move player
+
+        //borders
+        if (transform.position.x < -9 && !rightFace || transform.position.x > 9 && rightFace)
         {
             speed = 0;
         }
